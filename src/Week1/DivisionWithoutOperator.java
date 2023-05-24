d52d33eb4e5d6a58d3e46356098a477011cbe4b6
Dennis <dodabas1211@gmail.com>
2023-05-23 22:11:47 -0400

    wrote code for practice

--- src/Week1/DivisionWithoutOperator.java
+++ src/Week1/DivisionWithoutOperator.java
@@ -1,4 +1,28 @@
 package Week1;
 
 public class DivisionWithoutOperator {
-}
+
+
+    public static void main(String[] args) {
+
+            int num1 = 100;
+            int num2 = 10;
+            int num = 0;
+
+            while (num1>=num2){
+                num1 -= num2;
+                num++;
+            }
+
+            System.out.println(num);
+        }
+
+
+    }
+
+/*
+Numbers - - Divide without / operator
+Write a method that can divide two numbers without using division operator
+ */
+
+
--- src/Week1/oddOrEven.java
+++ src/Week1/oddOrEven.java
@@ -1,4 +1,41 @@
 package Week1;
 
+import java.util.Scanner;
+
 public class oddOrEven {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+
+        int number = input.nextInt();
+
+
+        if (number % 2 == 0) {
+            System.out.println("identity" + "(" + number + ")" + "->" + " Odd");
+        } else {
+            System.out.println("identity" + "(" + number + ")" + "->" + " Even");
+        }
+
+
+    }
 }
+
+
+
+
+
+
+/*
+1. Numbers -- odd & even
+Write a method which can identifies given number is even or odd
+
+EX:
+  identify(5) -> "Odd"
+  identify(6) -> "Even"
+ */
+
+
