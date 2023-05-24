ff3e25c893e6b3eea20f73055823812e98443ea9
Olga_Cydeo <relastasmart@gmail.com>
2023-05-24 15:29:35 -0400

    Added Divison, FINRA, oddOrEven methods

--- src/Week1/DivisionWithoutOperator.java
+++ src/Week1/DivisionWithoutOperator.java
@@ -1,4 +1,22 @@
+
 package Week1;
 
 public class DivisionWithoutOperator {
+
+    public static int divide(int num1, int num2){
+
+        int result = 0;
+
+        for(int i = 0; i <= num1; i++){
+            if( i * Math.abs(num2) == Math.abs(num1)){
+                result = i;
+            }
+
+            if((num1<0 && num2>0) || (num1>0 && num2 <0 )){
+                result -= (result*2);
+            }
+        }
+        return result;
+    }
+
 }
--- src/Week1/oddOrEven.java
+++ src/Week1/oddOrEven.java
@@ -1,4 +1,13 @@
 package Week1;
 
 public class oddOrEven {
+
+    public static String identify(int num) {
+
+        if(num % 2 == 0){
+            return "Even";
+        }
+        return "Odd";
+
+    }
 }
