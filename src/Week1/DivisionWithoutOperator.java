9326d22ab31b1a5f5f64d1ed43f104e049f0a76a
Van Gogh <fatihtoplu@Fatihs-MacBook-Pro.local>
2023-05-24 13:13:40 +0200

    Added the solution Division Without Operator

--- src/Week1/DivisionWithoutOperator.java
+++ src/Week1/DivisionWithoutOperator.java
@@ -1,4 +1,37 @@
 package Week1;
 
 public class DivisionWithoutOperator {
+
+    public static String divideWithoutOperator(int dividend, int divisor) {
+        // Convert the numbers to positive to simplify the calculation
+        int absDivisor = Math.abs(divisor);
+        int absDividend = Math.abs(dividend);
+
+        int temp = absDivisor;
+        int result = 0;
+
+        // Check for division by zero
+        if (divisor == 0) {
+            throw new ArithmeticException("divisor cannot be Zero");
+        } else if (dividend == 0 ){
+            return "0";
+        }
+
+        // Perform the division
+        while (absDividend >= temp) {
+
+            temp += absDivisor;
+            result++;
+
+        }
+
+        // Apply the sign to the result
+        if (dividend < 0)
+            result *= -1;
+        if (divisor < 0)
+            result *= -1;
+
+        return dividend + " divided by " + divisor + " is: " + result + " and remainder is " + (dividend - (result * divisor));
+
+    }
 }
