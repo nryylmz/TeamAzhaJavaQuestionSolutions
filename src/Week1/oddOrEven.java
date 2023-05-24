43d895acce895e1c0f956a8a654e2f718ee8d7e0
Filiz34 <balandifiliz@gmail.com>
2023-05-24 01:08:01 -0400

    added Odd or Even case

--- src/Week1/oddOrEven.java
+++ src/Week1/oddOrEven.java
@@ -2,6 +2,31 @@ package Week1;
 
 public class oddOrEven {
 
+    public static void main(String[] args) {
+
+        int num = 35 ; // given number
+
+        if(num % 3 == 0){ // if the given number divisible by 3
+            System.out.println("odd number"); // then it is an odd number
+        } else if (num % 2 == 0) { // if the given number is divisible by 2
+            System.out.println("even number"); // then it is an even number
+        }else{ // otherwise
+            System.out.println("invalid number"); // the given number is invalid.
+        }
+        System.out.println("================================================================");
+        // OR
+        int num2 = 26;
+
+        boolean isOdd = num2 % 3  == 0;
+
+        boolean isEven = !isOdd;
+
+        System.out.println(num2+ " is an even number "+ isEven);
+        System.out.println(num2+ " is an odd number "+ isOdd);
+
+    }
+
+
 
 }
 
