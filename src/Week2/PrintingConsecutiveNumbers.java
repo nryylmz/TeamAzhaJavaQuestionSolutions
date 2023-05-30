dd28c087e68b2ea768edfe8c307a17081a96550d
Cydeo-Nuray <yilmaz.nn@gmail.com>
2023-05-30 00:31:50 -0400

    Week2 questions answered.

--- src/Week2/PrintingConsecutiveNumbers.java
+++ src/Week2/PrintingConsecutiveNumbers.java
@@ -1,4 +1,41 @@
 package Week2;
 
+
 public class PrintingConsecutiveNumbers {
+    public static void printingConsecutiveNumbers(int N) {//created a method with 'int N' parameter
+
+        for (int i = 1; i <= N; i++) {//created for loop to iterate numbers 1-N; initialized 1 and conditioned to N
+
+                if( i % 2==0 && i % 3==0 && i % 5==0 ){//If i is divisible by 2, 3, and 5
+                    System.out.println("CodilityTestCoders"); /*the statement will print the concatenation of the
+                                                                respective words Codility, Test & Coders */
+                }else if( i % 2 == 0 && i % 3==0 ) {// Else if i is divisible by both 2 and 3
+                    System.out.println("CodilityTest");// 'CodilityTest' will be printed.
+                }else if( i % 2 == 0 && i % 5==0) {//Else if i is divisible by both 2 and 5
+                    System.out.println("CodilityCoders");// 'CodilityCoders' will be printed
+                }else if ( i % 3==0 && i% 5== 0 ) {//Else if i is divisible by both 3 and 5
+                    System.out.println("TestCoders");//'CodilityCoders' will be printed
+                }else if( i % 2 == 0 ) {//Else if i is divisible by 2
+                    System.out.println("Codility");//'Codility' will be printed
+                }else if( i % 3 == 0 ) {//Else if i is divisible by 3
+                    System.out.println("Test");//Test will be printed
+                }else if( i % 5 == 0 ) {//Else if i is divisible by 5
+                    System.out.println("Coders");//'Coders' will be printed
+                }else{//Else none of the conditions are met
+                    System.out.println(i);//the value of 'i' will be printed
+                }
+
+            }
+        }
+
+        public static void main(String[] args) {//executed the method using the example given in the task.
+        int N = 17;
+        printingConsecutiveNumbers(N);
+    }
 }
+
+
+
+
+
+
--- src/Week2/SwapNumbers.java
+++ src/Week2/SwapNumbers.java
@@ -1,4 +1,21 @@
 package Week2;
-
 public class SwapNumbers {
+    public static void main(String[] args) {
+        int num = 345, num1 = 567;//declared two int variables 'num' and 'num1' with values 345 and 567, respectively
+
+        num = num + num1;//used the arithmetic operator '+' to add the values of num and num1 and assigned to num
+        //num= 345+567; The value of num variable becomes 912.
+        num1 = num - num1;// used the arithmetic operator '-' to subtract the value of num1 from num & assigned to num1
+        // num1= 912-567; The value of num1 changes from 912 to 345.
+        num = num - num1;//used the arithmetic operator - to subtract the value of num from num1 and assigned to back to
+        // num= 912-345; The value of the num variable changes again, but this time to the value of 567.
+
+
+        System.out.println("num = " + num);
+        System.out.println("num1 = " + num1);
+
+
+
+
+    }
 }
