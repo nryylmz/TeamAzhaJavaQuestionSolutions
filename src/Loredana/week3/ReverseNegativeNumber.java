2ecd3f740617cc1333753b0ed69bfe883cf81e0c
Loredana <samesioana@gmail.com>
2023-06-07 21:57:36 -0500

    Week 3 - Java problems - solutions

--- src/Loredana/week3/ReverseNegativeNumber.java
+++ src/Loredana/week3/ReverseNegativeNumber.java
@@ -8,10 +8,10 @@ public class ReverseNegativeNumber
     {
         int reverse = 0;
 
-            while (n!=0)
+            while (n != 0)
             {
-                reverse = reverse*10 + n%10;
-                n/=10;
+                reverse = reverse * 10 + n % 10;
+                n /= 10;
                 
             }
         return reverse;
