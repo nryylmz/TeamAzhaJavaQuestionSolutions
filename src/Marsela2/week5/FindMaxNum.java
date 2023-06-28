b251363e9d3dd7cb3d01387cfc610e2e5a329749
Marsela_Cydeo <marsela.gramshi@gmail.com>
2023-06-27 20:37:14 -0400

    Updated code for MaxNum

--- src/Marsela2/week5/FindMaxNum.java
+++ src/Marsela2/week5/FindMaxNum.java
@@ -1,32 +1,28 @@
 package Marsela2.week5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindMaxNum {
 
-    public static void main(String[] args) {
+    public static int maxValue1(int[] n) {
+        int max = Integer.MIN_VALUE;
+        for (int each : n)
+            if (each > max)
+                max = each;
 
-        Scanner input = new Scanner(System.in);
+        return max;
+    }
 
-        int max = -2147483648;  // we assign the smallest int value
-
-        for(int i = 0; i <5; i++ ){
-            System.out.println("Enter a number: ");
-            int num = input.nextInt();  // suppose user entered 10, 5, 3, 20, 15
-
-
-            if(num > max){        //  if the user enters a number greater than the current max number
-                max = num;       // then that number entered by user becomes the max number
-            }
-
-        }
-
-        System.out.println("max = " + max);
+    public static int maxValue2(int[] n) {
+        Arrays.sort(n);
+        return n[n.length - 1];
+    }
 
 
     }
 
-}
+
 
  //   Find Maximum:
    //     Write a method that can find the maximum number from an int Array.
